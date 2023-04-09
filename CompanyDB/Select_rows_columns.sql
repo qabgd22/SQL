1b73eca0bcb759c6e41aff00e954b9791df50eaa
SW Tester <swtest150@gmail.com>
2023-04-09 22:57:51 +0200

    2nd commit

--- CompanyDB/Select_rows_columns.sql
+++ CompanyDB/Select_rows_columns.sql
@@ -5,3 +5,10 @@ WHERE revenue < 5000000;
 
 SELECT * FROM company
 WHERE revenue > 5000000;
+
+SELECT * FROM company
+ORDER BY revenue
+LIMIT 1;
+
+SELECT * FROM company
+WHERE NOT revenue >= 5000000;
